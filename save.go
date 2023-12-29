0f948e71e56a87c41a225f6f0fb8f9c1b54bcce1
mohammed Tmeizeh <mohammedt.pal@gmail.com>
2023-12-29 17:53:16 +0200

    commit

--- save.go
+++ save.go
@@ -69,7 +69,7 @@ import (
 // DecodeBase64Chunks decodes base64-encoded chunks from a file and returns them as a byte array.
 func DecodeBase64Chunks(filePath string) ([]byte, error) {
 	content, err := ioutil.ReadFile(filePath)
-	fmt.Println("Size of content:", len(content))
+	fmt.Println("Size of rescived base64:", len(content))
 
 	if err != nil {
 		return nil, err
@@ -116,8 +116,8 @@ func EncodeToBase64Chunks(data []byte, chunkSize int) string {
 
 func main() {
 	// Replace "path/to/your/base64_chunks.txt" with the actual path to your file.
-	filePath := "2.txt"
-	outputFilePath := "f.txt"
+	filePath := "splitOutPut.txt"
+	outputFilePath := "GoOutPutBase64.txt"
 	// Decode base64-encoded chunks from the file.
 	decodedBytes, err := DecodeBase64Chunks(filePath)
 	if err != nil {
@@ -131,9 +131,10 @@ func main() {
 
 	// Encode the bytes to base64 and get a string where each line represents a base64-encoded chunk.
 	base64String := EncodeToBase64Chunks(decodedBytes, 2048) // 76 characters per line is a common line length for base64 encoding.
+	fmt.Println("Size returned base64:", len(base64String))
 
 	// Print the base64 string (optional).
-	fmt.Println("Base64 String:")
+	//fmt.Println("Base64 String:")
 	//fmt.Println(base64String)
 	err = ioutil.WriteFile(outputFilePath, []byte(base64String), 0644)
 	if err != nil {
