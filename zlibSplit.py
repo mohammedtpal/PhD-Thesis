6978465edfa5ac3ea14f245a2cdd512e32facc1e
mohammed Tmeizeh <mohammedt.pal@gmail.com>
2024-02-20 06:54:59 +0200

    commit

--- zlibSplit.py
+++ zlibSplit.py
@@ -9,22 +9,35 @@ def compress_and_split(input_file, chunk_size):
 
     # Compress the data
     compressed_data = zlib.compress(data)
+    original_size_bytes = len(data)
 
+    # Calculate the size of the compressed data
+    compressed_size_bytes = len(compressed_data)
+
+    print(f"Size of original data: {original_size_bytes} bytes")
+    print(f"Size of compressed data: {compressed_size_bytes} bytes")
+
+    # Compress the data
     # Split the compressed data into chunks
     chunks = [compressed_data[i:i+chunk_size] for i in range(0, len(compressed_data), chunk_size)]
+    total_size_bytes = sum(len(chunk) for chunk in chunks)
+
+   # print(f"Total size of all chunks: {total_size_bytes} bytes")
 
     return chunks
 
 def encode_chunks_to_base64(chunks):
     # Encode each chunk to Base64
     encoded_chunks = [base64.b64encode(chunk).decode('utf-8') for chunk in chunks]
+    total_size_bytes = sum(len(encoded_chunk.encode('utf-8')) for encoded_chunk in encoded_chunks)
+  #  print(f"Total size of encoded_chunks: {total_size_bytes} bytes")
 
     return encoded_chunks
 
 if __name__ == "__main__":
-    input_file = "OOP.zip"  # Replace with your actual file name
+    input_file = "OOP.pdf"  # Replace with your actual file name
     #chunk_size = 1024  # Set your desired chunk size
-    chunk_size = 4 * 1024  # 4KB in bytes 
+    chunk_size =  1024  # 4KB in bytes 
     # Check if the file exists
     if not os.path.isfile(input_file):
         print(f"Error: File '{input_file}' not found.")
